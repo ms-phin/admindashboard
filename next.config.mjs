cf45f8da38f6b39b70d2fe29c13dc301d381e610
mesfin beyene <mesfinbaby@gmail.com>
2024-08-12 21:37:28 -0700

    fix config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    runtime: "edge", // or 'nodejs' depending on your use case
+  },
+};
 
 export default nextConfig;
